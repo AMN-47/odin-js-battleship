224151e26e3b0edbbd17f42bd10da49295785745
AMN-47 <akhilnagar57@gmail.com>
2026-03-28 15:37:21 +0200

    Added index.js and configured webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,37 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
+export default {
+  mode: 'development',
+
+  entry: './src/index.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(process.cwd(), 'dist'),
+    clean: true,
+  },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    static: './dist',
+    hot: true,
+    open: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html',
+    }),
+  ],
+};
