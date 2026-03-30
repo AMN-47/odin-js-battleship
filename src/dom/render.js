1a0bee802e39194bbb920e310bd0e5d49a0aa962
AMN-47 <akhilnagar57@gmail.com>
2026-03-30 18:33:31 +0200

    fetched ID and created game div

--- src/dom/render.js
+++ src/dom/render.js
@@ -1,3 +1,9 @@
 export function renderBoards(game) {
+    const container = document.getElementById('game');
+    container.innerHTML = '';
+
+    const playerBoard = document.createElement('div');
+    playerBoard.textContent = 'Player Board';
+
     
 }
