14243781a207d8aa7e18a618d8e30b563153a2bd
AMN-47 <akhilnagar57@gmail.com>
2026-03-31 19:50:59 +0200

    updated Render file

--- src/dom/render.js
+++ src/dom/render.js
@@ -1,13 +1,10 @@
 export function renderBoards(game) {
-    const container = document.getElementById('game');
-    container.innerHTML = '';
+  const container = document.getElementById('game');
+  container.innerHTML = '';
 
-    const playerBoard = document.createElement('div');
-    playerBoard.textContent = 'Player Board';
+  const playerBoardEl = createBoard(game.player.board, false);
+  const enemyBoardEl = createBoard(game.computer.board, true, game);
 
-    const enemyBoard = document.createElement('div');
-    enemyBoard.textContent = 'Enemy Board';
-
-    container.appendChild(playerBoard);
-    container.appendChild(enemyBoard);
+  container.appendChild(playerBoardEl);
+  container.appendChild(enemyBoardEl);
 }
