e400156a114eb61ec2dda34d9d2d1faf8f6a429d
AMN-47 <akhilnagar57@gmail.com>
2026-03-31 19:39:27 +0200

    Added cordinates of player and computer ships

--- src/modules/GameController.js
+++ src/modules/GameController.js
@@ -2,5 +2,16 @@ import Player from './Player.js';
 
 export default function GameController() {
     const player = new Player();
-    const computer = new Player(True);
+    const computer = new Player(True); 
+
+    let gameOver = false;
+
+    //Temp: Preset Ships
+    function setupGame() {
+        player.board.placeShip(0, 0, 3);
+        player.board.placeShip(2, 2, 2);
+
+        computer.board.placeShip(1, 1, 3);
+        computer.board.placeShip(4, 4, 2);
+    }
 }
