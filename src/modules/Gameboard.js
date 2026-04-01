8891c6e3aaa4bdd1e8dcf9441c07395957ab06f9
AMN-47 <akhilnagar57@gmail.com>
2026-04-01 20:11:31 +0200

    created fpr loop for grid elements

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,4 +1,8 @@
 function createBoard(board, isEnemy = false, game = null) {
     const boardEl = document.createElement('div');
     boardEl.classList.add('board');
+
+    for (let y =0; y < 10; y++) {
+        for (let x = 0; x < 10; x++) {}
+    }
 }
